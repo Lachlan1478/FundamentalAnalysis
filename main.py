65ce85ecb0d4a85db9f68972e156fa1301c4497a
Lachy1478 <gaylerlachlan@gmail.com>
2022-11-24 09:07:17 +1030

    Functional Data collection. Note: Improvements of efficiency should be made to the calculation of the historical annual returns.

--- main.py
+++ main.py
@@ -4,14 +4,28 @@ import yfinance as yf
 import Gather_Data
 import ExpectedReturnRisk as Ex
 import statistics
+import numpy as np
 
-Companies = ['ORG.AX', 'WDS.AX']
+Path = "ALLORDS_Tickers.txt"
+
+Read = open(Path, "r")
+for i in Read:
+    Tickers = [line.strip('"'+'\n') for line in open(Path)]
+
+Length = len(Tickers)
+for i in range(0, Length):
+    Tickers[i] = Tickers[i]+'.AX'
+
+
+#Companies = ['ORG.AX', 'WDS.AX']
+
 Metrics = ['trailingEps', 'forwardEps', 'enterpriseToRevenue', 'ebitda', 'returnOnAssets',
            'returnOnEquity', 'debtToEquity', 'heldPercentInstitutions', 'priceToBook',
            'enterpriseValue', 'marketCap', 'operatingCashflow', 'averageVolume']
 CompanyMetrics = {}
-
-for company in Companies:
+i = 0
+for company in Tickers:
+    print("Iteration ", i, ": begin")
 
     CompanyMetrics[company] = {}
     CompanyInfo = yf.Ticker(company)
@@ -20,7 +34,7 @@ for company in Companies:
         try:
             CompanyMetrics[company][metric] = CompanyInfo.info[metric]
         except:
-            CompanyMetrics[company][metric] = "NA"
+            CompanyMetrics[company][metric] = np.nan
 
     try:
         CompanyPrice = Gather_Data.GatherData(company, "1d", "1d")["Close"][0]
@@ -28,19 +42,20 @@ for company in Companies:
         CompanyMetrics[company]['trailingPE'] = CompanyMetrics[company]['PricePerShare'] / CompanyMetrics[company]['trailingEps']
         CompanyMetrics[company]['forwardPE'] = CompanyMetrics[company]['PricePerShare'] / CompanyMetrics[company]['forwardEps']
     except:
-        CompanyMetrics[company]['trailingPE'] = "NA"
-        CompanyMetrics[company]['forwardPE'] = "NA"
+        CompanyMetrics[company]['trailingPE'] = np.nan
+        CompanyMetrics[company]['forwardPE'] = np.nan
 
     try:
         closeData = Gather_Data.GatherData(company, "10y", "1d")
         CompanyMetrics[company]['HistoricalReturn'] = statistics.mean(Ex.ExpectedReturn(closeData))
         CompanyMetrics[company]['HistoricalReturnRisk'] = Ex.ExpectedReturnRisk(Ex.ExpectedReturn(closeData))
     except:
-        CompanyMetrics[company]['HistoricalReturn'] = "NA"
-        CompanyMetrics[company]['HistoricalReturnRisk'] = "NA"
+        CompanyMetrics[company]['HistoricalReturn'] = np.nan
+        CompanyMetrics[company]['HistoricalReturnRisk'] = np.nan
+    print("Iteration ", i, ": end")
+    i = i+1
 
 df = pd.DataFrame.from_dict(CompanyMetrics).T
-print(df)
 
 sortbyPE = df.sort_values(by = 'forwardPE')
 print(sortbyPE)
